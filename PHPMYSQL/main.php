002fdf7c82b5a99e02662065005052efc23ddac0
Gladiat04 <gabriele.losurdo10@gmail.com>
2022-04-17 12:06:23 +0200

    CSS aggiunto alla pagina index.php, possibile effettuare login e signup!

--- PHPMYSQL/main.php
+++ PHPMYSQL/main.php
@@ -17,51 +17,106 @@
         $password = "";
         $database = "provalogin";
         $connection = new mysqli($host,$user,$password,$database);
-        $sql = "INSERT INTO utenti(nome,cognome,email) VALUES( ?, ?, ?);";
-        if($statement = $connection->prepare($sql)){
-            $statement -> bind_param("sss",$nome,$cognome,$email);
+        if($_POST["submit"]==="SignUp"){
+            $sql = "INSERT INTO utenti(username,email,pw) VALUES( ?, ?, ?);";
+            if($statement = $connection->prepare($sql)){
+                $statement -> bind_param("sss",$username,$email,$pw);
 
-            $nome = $_POST['nome'];
-            $cognome = $_POST['cognome'];
-            $email = $_POST['e-mail'];
-            $sql2 = "SELECT * FROM utenti WHERE email ='$email';";
-            if($result2 = $connection -> query($sql2)){
-                if($result2->num_rows > 0){
-                    echo "Hai già creato un account con questa email.
-                    <a href='signin.php' >Torna alla pagina iniziale</a>
-                    ";
+                $username = $_POST['username'];
+                $email = $_POST['e-mail'];
+                $pw = $_POST['password'];
+                $cpw = $_POST['cpassword'];
+                if($pw!=$cpw){
+                    header('HTTP/1.1 301 Moved Permanently');
+                    header('Location: http://localhost/PHPMYSQL/index.php?value=SignUp');
                 }else{
-                    $statement -> execute();
+                    $sql2 = "SELECT * FROM utenti WHERE email ='$email' or username ='$username';";
+                    if($result2 = $connection -> query($sql2)){
+                        if($result2->num_rows > 0){
+                            echo "E stato gia creato un account con questa email o username!<br>
+                            <a href='index.php' >Torna alla pagina iniziale</a>
+                            ";
+                        }else{
+                            $statement -> execute();
+                            $sql3 = "SELECT * FROM utenti;";
+                            if($result = $connection -> query($sql3)){
+                                if($result->num_rows > 0){
+                                    echo '<table><thead>
+                                    <tr>
+                                    <th>Id</th>
+                                    <th>Username</th>
+                                    <th>E-mail</th>
+                                    <th>Password</th>
+                                    </tr></thead><tbody>
+                                    ';
+                                    while($row = $result->fetch_array()){
+                                        echo '
+                                        <tr>
+                                        <td>'.$row['id'].'</td>
+                                        <td>'.$row['username'].'</td>
+                                        <td>'.$row['email'].'</td>
+                                        <td>'.$row['pw'].'</td>
+                                        </tr>
+                                        ';
+                                    }
+                                    echo '</tbody></table>';
+                                }
+                            }
+    
+                        }
+                    }
+
+                }
+            }
+            $statement -> close();
+        }else if($_POST["submit"]==="LogIn"){// login
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $sql = "SELECT * FROM utenti WHERE username = '$username'";
+            if($result = $connection -> query($sql)){
+                if($result ->num_rows > 0){
+                    $row = $result->fetch_array();
+                    if($row['pw']==$password){
+                        echo 'Benvenuto!
+                            Username: '.$row['username'].'<br>
+                            E-mail: '.$row['email'].'<br>
+                            Password: '.$row['pw'].'<br>'
+                        ;
+                    }
+                    echo "------------------------------------------------------------------------------------------------------------------------";
                     $sql = "SELECT * FROM utenti;";
                     if($result = $connection -> query($sql)){
                         if($result->num_rows > 0){
                             echo '<table><thead>
                             <tr>
                             <th>Id</th>
-                            <th>Nome</th>
-                            <th>Cognome</th>
+                            <th>Username</th>
                             <th>E-mail</th>
+                            <th>Password</th>
                             </tr></thead><tbody>
                             ';
                             while($row = $result->fetch_array()){
                                 echo '
                                 <tr>
                                 <td>'.$row['id'].'</td>
-                                <td>'.$row['nome'].'</td>
-                                <td>'.$row['cognome'].'</td>
+                                <td>'.$row['username'].'</td>
                                 <td>'.$row['email'].'</td>
+                                <td>'.$row['pw'].'</td>
                                 </tr>
                                 ';
                             }
                             echo '</tbody></table>';
                         }
                     }
-
+                }else{
+                    echo 'Non esiste nessun utente con questo username!<br>
+                    <a href="index.php?value=SignIn">Torna al pagina di Login!</a>
+                    ';
                 }
             }
         }
+        
 
-        $statement -> close();
 
         $connection->close();
     ?>
