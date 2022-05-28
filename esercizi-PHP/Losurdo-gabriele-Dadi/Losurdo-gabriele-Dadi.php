1d75cf3426d05e07bc692ee93bb02fc2ce88aca5
Gladiat04 <gabriele.losurdo10@gmail.com>
2022-05-28 14:37:35 +0200

    Exs

---
+++ esercizi-PHP/Losurdo-gabriele-Dadi/Losurdo-gabriele-Dadi.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Losurdo-gabriele-Dadi</title>
+</head>
+<body>
+            
+    <h1>Lancio dei dadi</h1><br>
+    <?php
+
+        if(!isset($_POST["invio"])){
+            index();
+        }else{
+            ldadi();
+        }
+
+        // index - con questa funzione chiedo all'utente quanti dadi lanciare
+        function index(){
+
+            echo <<< FINE
+            <form action="" method="POST">
+            <b>Quanti dadi vuoi lanciare?</b><br>
+            <input type="number" name="ndadi">
+            <input type="submit" name="invio" value="Invia!">
+            </form>
+
+            FINE;
+
+        }
+        // fine index 
+
+        // ladadi - funzione per il lancio random dei dadi e calcolo del punteggio
+        function ldadi(){
+
+            $ndadi = (int)$_POST["ndadi"]; // questa variabile conterrà il numero di dadi che l'utente vuole lanciare
+            $sommaP=0; // variabile che conterrà la somma totale dei punti
+            $nc=0;
+
+            $im_dadi  = array(
+                1=>"dadi-200px/dado_1.png",
+                2=>"dadi-200px/dado_2.png",
+                3=>"dadi-200px/dado_3.png",
+                4=>"dadi-200px/dado_4.png",
+                5=>"dadi-200px/dado_5.png",
+                6=>"dadi-200px/dado_6.png"
+            );
+            // con questo ciclo viene 'lanciato' un dado, verrà aggiunto il valore del dado alla variabile sommaP (somma totale dei punti)
+            // e infine sarà data in output l'immagine del dado lanciato
+            for($i=0;$i<$ndadi;$i++){
+                $nc=mt_rand(1,6);
+                $sommaP+=$nc;
+                echo '<img src='.$im_dadi[$nc].'>';
+                
+            }
+            // output dei punti effettuati con il lancio dei dadi
+            echo "<br>Hai effettuato ".$sommaP." punti con ".$ndadi." dadi!";
+        }
+        // fine ldadi
+
+    ?>
+</body>
+</html>
