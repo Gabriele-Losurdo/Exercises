1082cffad96cef5c1ddbfac2bef536a70032d1d1
Gabriele-Losurdo <gabriele.losurdo10@gmail.com>
2022-11-04 18:51:55 +0100

    Almost done

--- Esercizi-JS/Losurdo-gabriele_Schede-PrtE-commerce/script.js
+++ Esercizi-JS/Losurdo-gabriele_Schede-PrtE-commerce/script.js
@@ -1,5 +1,5 @@
 let find = document.getElementById("find");
-let search = document.getElementById("cercaprodotto");
+let search = document.getElementById("search");
 
 find.addEventListener('click', function() {
     let requestURL = "https://dummyjson.com/products/search?q=" + search.value;
@@ -11,11 +11,11 @@ find.addEventListener('click', function() {
     request.onload = function() {
         let prodotti = request.response;
         for (let i = 0; i < 100; i++) {
-            document.getElementById("nomeprodotto").innerHTML = prodotti.products[i].title;
-            document.getElementById("descrizione").innerHTML = prodotti.products[i].description;
-            document.getElementById("prezzo").innerHTML = prodotti.products[i].price;
-            document.getElementById("categoria").innerHTML = prodotti.products[i].category;
-            document.getElementById("immagini").innerHTML = prodotti.products[i].images[0];
+            document.getElementById("product-title").innerHTML = prodotti.products[i].title;
+            document.getElementById("product-description").innerHTML = "Descrizione: " + prodotti.products[i].description;
+            document.getElementById("product-price").innerHTML = "Prezzo: " +prodotti.products[i].price;
+            document.getElementById("product-category").innerHTML = "Categoria: " +prodotti.products[i].category;
+            // document.getElementById("immagini").innerHTML = prodotti.products[i].images[0];
         }
     }
 });
