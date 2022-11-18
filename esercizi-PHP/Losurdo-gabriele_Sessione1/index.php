9fadf4d8e171bcb9fa57d6890046b111f704a6e0
Gabriele-Losurdo <gabriele.losurdo10@gmail.com>
2022-11-18 17:15:00 +0100

    Session Ex1

--- esercizi-PHP/Losurdo-gabriele_Sessione1/index.php
+++ esercizi-PHP/Losurdo-gabriele_Sessione1/index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+<!--===============================================================================================--> 
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
@@ -29,41 +29,129 @@
 </head>
 <body>
 	
-	<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form" method="post" action=""> 
-					<span class="login100-form-logo">
-						<img class="Photo" src="images/avatar.png">
-					</span>
+<?php
+if(!isset($_POST["login"])){
+	session_start();
+	if(isset($_POST["logout"])){
+		logout();
+	}else{
+		index();
+	}
+}else{
+	login();
+}
 
-					<span class="login100-form-title p-b-34 p-t-27">
-						Ciao
-					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
-						<span class="focus-input100" data-placeholder="&#xf207;"></span>
-					</div>
+function index(){ // initial form
+	echo <<< END
+		<div class="limiter">
+			<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+				<div class="wrap-login100">
+					<form class="login100-form validate-form" method="post" action=""> 
+						<span class="login100-form-logo">
+							<img class="Photo" src="images/avatar.png">
+						</span>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100" data-placeholder="&#xf191;"></span>
-					</div>
+						<span class="login100-form-title p-b-34 p-t-27">
+							Log in
+						</span>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							logout
-						</button>
-					</div>
-				</form>
+						<div class="wrap-input100 validate-input" data-validate = "Enter username">
+							<input class="input100" type="text" name="username" placeholder="Username">
+							<span class="focus-input100" data-placeholder="&#xf207;"></span>
+						</div>
+
+						<div class="wrap-input100 validate-input" data-validate="Enter password">
+							<input class="input100" type="password" name="pass" placeholder="Password">
+							<span class="focus-input100" data-placeholder="&#xf191;"></span>
+						</div>
+
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+						</div>
+
+						<div class="container-login100-form-btn">
+							<button type="submit" name="login" class="login100-form-btn">
+								Login
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
-	</div>
-	
+	END;
+}
 
-	<div id="dropDownSelect1"></div>
-	
+function login(){ // login
+
+	$username = $_POST["username"];
+	$password = $_POST["pass"];
+
+	//Salvo i dati nella sessione
+	$_SESSION['username'] = $username;
+	$_SESSION['password'] = $password;
+
+	echo <<< END
+
+	<div class="limiter">
+			<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+				<div class="wrap-login100">
+					<form class="login100-form validate-form" method="post" action=""> 
+						<span class="login100-form-logo">
+							<img class="Photo" src="images/avatar.png">
+						</span>
+
+						<span class="login100-form-title p-b-34 p-t-27">
+							Benvenuto $username, login effettuato
+						</span>
+
+						<div class="container-login100-form-btn">
+							<button type="submit" name="logout" class="login100-form-btn">
+								Logout
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+	END;
+}
+
+function logout(){ // logout
+	session_unset(); // cancello tutte le variabili della sessione
+
+	echo <<< END
+
+	<div class="limiter">
+			<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+				<div class="wrap-login100">
+					<form class="login100-form validate-form" method="post" action=""> 
+						<span class="login100-form-logo">
+							<img class="Photo" src="images/avatar.png">
+						</span>
+
+						<span class="login100-form-title p-b-34 p-t-27">
+							Logout effettuato
+						</span>
+
+						<div class="container-login100-form-btn">
+							<button type="submit" name="Bye" class="login100-form-btn">
+								Torna al login
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+	END;
+}
+?>
+		
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
