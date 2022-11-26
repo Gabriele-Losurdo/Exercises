c2b7d822669cb59d385cda42ed0e5e26285d85ad
Gabriele-Losurdo <gabriele.losurdo10@gmail.com>
2022-11-26 12:09:49 +0100

    Almost done

--- esercizi-PHP/Losurdo-gabriele_Campionato/index.php
+++ esercizi-PHP/Losurdo-gabriele_Campionato/index.php
@@ -24,28 +24,23 @@
             index();
         }
     }else{
+        $classifica[] = $_SESSION["classifica"]; // recupera l'array salvato
+        $classifica[] = array(
+            "Name" => $_POST["Name"],
+            "Vittorie" => $_POST["Vittorie"],
+            "Sconfitte" => $_POST["Sconfitte"],
+            "Pareggi" => $_POST["Pareggi"],
+            "Punti" => $_POST["Punti"]
+        );
+        $_SESSION["classifica"] = $classifica;
         index();
     }
 
     function index(){
         $classifica[] = $_SESSION["classifica"]; // recupera l'array salvato
-        $nome = $_POST["Name"];
-        $vitt = $_POST["Vittorie"];
-        $sconf = $_POST["Sconfitte"];
-        $par = $_POST["Pareggi"];
-        $punti = $_POST["Punti"];
-        $classifica[] = array(
-            "Name" => $nome,
-            "Vittorie" => $vitt,
-            "Sconfitte" => $sconf,
-            "Pareggi" => $par,
-            "Punti" => $punti
-        );
-        $_SESSION["classifica"] = $classifica;
-
         echo <<< END
         
-        <form method="POST">
+        <form action="" method="POST">
             <div class="dati" >
                 
                 <div class="mb-3">
@@ -55,22 +50,22 @@
         
                 <div class="mb-3">
                     <label class="form-label">Vittorie: </label>
-                    <input type="text" class="form-control" name="Vittorie">
+                    <input type="number" class="form-control" name="Vittorie">
                 </div>
         
                 <div class="mb-3">
                     <label class="form-label">Sconfitte: </label>
-                    <input type="text" class="form-control" name="Sconfitte">
+                    <input type="number" class="form-control" name="Sconfitte">
                 </div>
         
                 <div class="mb-3">
                     <label class="form-label">Pareggi: </label>
-                    <input type="text" class="form-control" name="Pareggi">
+                    <input type="number" class="form-control" name="Pareggi">
                 </div>
         
                 <div class="mb-3">
                     <label class="form-label">Punti classifica: </label>
-                    <input type="text" class="form-control" name="Punti">
+                    <input type="number" class="form-control" name="Punti">
                 </div>
         
           </div>
@@ -85,24 +80,24 @@
           <thead>
             <tr>
               <th>Squadra
-              <th>Vittore
+              <th>Vittorie
               <th>Pareggi
               <th>Sconfitte
               <th>Punti
               <th>Modifica
           </thead>
           <tbody>
-            <tr>
         END;
         for($i=0;$i<count($classifica);$i++){
-            foreach($classifica as $key=>$value){
+            echo "<tr>";
+            foreach($classifica[$i] as $key=>$value){
         
-            echo <<< END
-                <td>$value
-            END;
+                echo "<td>$value";
+        
             }
                 
-                echo ' <td><form method="POST"><button type="submit" class="btn btn-success" name="Modifica">Invia</button></form>';
+            echo ' <td><form action="" method="POST"><button type="submit" class="btn btn-success" name="Modifica">Invia</button>'.
+                     ' <input type="number" name="indice" value="$i" style="display:none;"></form>';
         }
         echo <<< END
           </tbody>
