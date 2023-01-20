3b6b455d449ecb9cc84a35948aca062e0601251e
claudiogame27 <claudio.visceglia27@gmail.com>
2023-01-20 09:52:04 +0100

    Update php and style

--- esercizi-PHP/Rubrica0.1/controller/Controller.php
+++ esercizi-PHP/Rubrica0.1/controller/Controller.php
@@ -1,5 +1,28 @@
 <?php        
-    
-
-    
+    if(isset($_POST['Contatto'])){
+        include('pages/consegna.php');
+    }else if(!isset($_GET["Rubrica"])){
+        if(isset($_POST['Aggiorna'])){
+            $Rubrica = $_SESSION["Rubrica"]; // recupera l'array salvato
+            $Rubrica[] = array( // aggiungo nell'array classifica un array associativo con i varie chiavi e valori inviati nel form
+                "Nome" => $_POST["Nome"],
+                "Cognome" => $_POST["Cognome"],
+                "Indirizzo" => $_POST["Indirizzo"],
+                "Città" => $_POST["Città"],
+                "Provincia" => $_POST["Provincia"],
+                "Cellulare" => $_POST["Cellulare"],
+                "Email" => $_POST["Email"]
+            );
+            $_SESSION["Rubrica"] = $Rubrica; // aggiorno la variabile di sessione
+            include('pages/Contatto.php');
+        }else if(!isset($_GET["add"])){ // viene eseguita solo la prima volta
+                $Rubrica=array();                  // Crea l'array vuoto
+                $_SESSION["Rubrica"] = $Rubrica;
+                include('pages/Contatto.php');
+        }else{
+            include('pages/Contatto.php');
+        } 
+    }else{
+        include('home.php');
+    }
 ?>
--- esercizi-PHP/Rubrica0.1/pages/Contatto.php
+++ esercizi-PHP/Rubrica0.1/pages/Contatto.php
@@ -1,29 +1,62 @@
 <?php $Rubrica = $_SESSION["Rubrica"]; ?>
 <div class="Contatto">
-    <h2>Aggiungi un nuovo contatto</h2>
+    <h2>I tuoi contatti:</h2>
+         <?php if(count($Rubrica) != 0){ ?>
+        <table class="table table-success table-striped-columns">
+                <tr  class="table-success">
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Cognome</th>
+                    <th>Indirizzo</th>
+                    <th>Città</th>
+                    <th>Provincia</th>
+                    <th>Cellulare</th>
+                    <th>Email</th>    
+                </tr>
+                <?php for($i=0;$i<count($Rubrica);$i++){ ?> 
+                    <tr  class="table-success">
+                        <td><?php echo $i ?></td>
+                    <?php foreach($Rubrica[$i] as $key=>$value){?>
+                
+                    <td><?php echo $value ?></td>  
+                
+                    <?php } ?>
+                    </tr>
+                <?php } ?>
+        </table>
+    <?php }else{ ?>
         <div class="alert alert-danger" role="alert">
-            <h3>Non sono ancora stati aggiunti dei contatti.</h3>
+            Non sono ancora stati aggiunti dei prodotti.
         </div>
+    <?php } ?>
         <form action="" method="post">
             <div class="mb-3">
                 <label for="Nome" class="form-label">Nome</label>
                 <input type="text" class="form-control" name="Nome"  required>
             </div>
             <div class="mb-3">
+                <label for="Città" class="form-label">Cognome</label>
+                <input type="text" class="form-control" name="Cognome" required>
+            </div>
+            <div class="mb-3">
                 <label for="Indirizzo" class="form-label">Indirizzo</label>
-                <input type="text" class="form-control" name="indirizzo" required>
+                <input type="text" class="form-control" name="Indirizzo" required>
             </div>
             <div class="mb-3">
                 <label for="Città" class="form-label">Città</label>
-                <input type="text" class="form-control" name="citta" required>
+                <input type="text" class="form-control" name="Città" required>
+            </div>
+            <div class="mb-3">
+                <label for="Città" class="form-label">Provincia</label>
+                <input type="text" class="form-control" name="Provincia" required>
             </div>
             <div class="mb-3">
                 <label for="Cellulare" class="form-label">Cellulare</label>
-                <input type="decimal" class="form-control" name="cellulare" required>
+                <input type="decimal" class="form-control" name="Cellulare" required>
             </div>
             <div class="mb-3">
                 <label for="Email" class="form-label">Email</label>
-                <input type="text" class="form-control" name="Email" required>
+                <input type="Email" class="form-control" name="Email" required>
             </div>
             <button type="submit" name="Aggiorna" value="true" class="btn btn-success">Invia</button>
             <button type="reset" name="Reset" class="btn btn-danger">Cancella dati</button>
