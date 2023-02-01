63231781bdf8cfb3577c77fe5bdf41955def31d5
Gabriele Losurdo <gabriele.losurdo10@gmail.com>
2023-02-01 15:42:04 +0100

    Film v 2.0(fix)

--- esercizi-PHP/Film/pages/Form.php
+++ esercizi-PHP/Film/pages/Form.php
@@ -3,27 +3,27 @@
   <form method="post">
     <div class="form-group">
       <label for="Titolo"><b>Titolo</b></label>
-      <input type="name" class="form-control" name="film[titolo]">
+      <input type="name" class="form-control" name="film[titolo]" required>
     </div>
     <div class="form-group">
       <label for="Nome_Regista"><b>Nome_Regista</b></label>
-      <input type="name" class="form-control" name="film[nome_regista]">
+      <input type="name" class="form-control" name="film[nome_regista]" required>
     </div>
     <div class="form-group">
       <label for="Descrizione"><b>Descrizione</b></label>
-      <input type="text" class="form-control" name="film[descrizione]">
+      <input type="text" class="form-control" name="film[descrizione]" required>
     </div>
     <div class="form-group">
       <label for="Durata_Film"><b>Durata_Film</b></label>
-      <input type="time" class="form-control" name="film[durata_film]">
+      <input type="time" class="form-control" name="film[durata_film]" required>
     </div>
     <div class="form-group">
       <label for="Anno_Pubblicazione"><b>Anno_Pubblicazione</b></label>
-      <input type="date" class="form-control" name="film[anno_pubblicazione]">
+      <input type="date" class="form-control" name="film[anno_pubblicazione]" required>
     </div>
     <div class="form-group">
       <label for="Genere"><b>Genere</b></label>
-      <input type="text" class="form-control" name="film[genere]">
+      <input type="text" class="form-control" name="film[genere]" required>
     </div><br>
     <button type="submit" name="aggiungi" class="btn btn-success">Invio</button>
     <button type="reset" class="btn btn-warning">Cancella</button>
